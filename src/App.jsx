97204051b3037c8dbb5f28d4116d6b3c8b271787
morgantstewart <morgan@paysigil.com>
2025-08-07 14:58:06 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// src/App.jsx
+
+import './App.css';
+
+const App = () => {
+  return (
+    <> 
+      <nav id="top-navbar">
+        <a href="/">Home</a>
+        <a href="/about-us">About Us</a>
+        <a href="/money-pit">Investment Opportunities</a>
+        <a href="/the-fine-print">Terms of Service</a>
+      </nav>
+      <main>
+        <h1>Welcome to Hyperbo.ly</h1>
+        <h2>The future is firmly in front of us!</h2>
+        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+      </main>
+    </>
+  );
+};
+
+export default App;
