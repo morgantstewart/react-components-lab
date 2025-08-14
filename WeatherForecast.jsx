e99b0655806e4dbd525389744d17ac19157366f6
morgantstewart <morgan@paysigil.com>
2025-08-14 01:15:16 -0400

    completed exercise

--- WeatherForecast.jsx
+++ WeatherForecast.jsx
@@ -1,8 +1,14 @@
 import './WeatherForecast.css';
 
-<div className="weather">
-  <h2>Day of the Week</h2>
-  <img src="" alt="" />
-  <p><span>conditions: </span>current weather conditions</p>
-  <p><span>time: </span>time of day</p>
-</div>
+const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
+  return (
+    <div className="weather">
+      <h2>{day}</h2>
+      <img src={img} alt={imgAlt} />
+      <p><span>conditions: </span>{conditions}</p>
+      <p><span>time: </span>{time}</p>
+    </div>
+  );
+};
+
+export default WeatherForecast;
